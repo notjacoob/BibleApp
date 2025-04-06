77640157859bd6ce5952f7a61445b91d3cb52a12
notjacoob <ds0293097@gmail.com>
2025-04-06 02:00:35 -0700

    DbAccessor.cs documentation

--- BibleApp/data/DbAccessor.cs
+++ BibleApp/data/DbAccessor.cs
@@ -1,7 +1,15 @@
 ﻿namespace BibleApp.data;
-
+/// <summary>
+/// dedicated class to provide DB strings for different environments
+/// </summary>
+/// <param name="hostEnvironment">injected</param>
+/// <param name="configuration">injected</param>
 public class DbAccessor(IHostEnvironment hostEnvironment, IConfiguration configuration)
 {
+    /// <summary>
+    /// gets a connection string from appsettings.json based on the environment
+    /// </summary>
+    /// <returns>either the development or production connection strings</returns>
     public string? GetConnectionString()
     {
         string connStr;
